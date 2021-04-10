c2e758ec3ff1625d03e29e22dc2e0df3617a959e
Michal Babiarz <michal.babiarz84@gmail.com>
2021-04-10 20:23:49 +0200

    Adds new aliases

--- git_aliases.sh
+++ git_aliases.sh
@@ -14,9 +14,12 @@ echo "*** Setting  git aliases ***"
 git config --global alias.co checkout
 git config --global alias.cob 'checkout -b'
 git config --global alias.br branch
+git config --global alias.dlocbr 'git branch -d'
+git config --global alias.drembr 'git push origin --delete'
 git config --global alias.fe fetch
 git config --global alias.pl pull
 git config --global alias.ph push
+git config --global alias.phf 'push -f'
 git config --global alias.ci 'commit -m'
 git config --global alias.fastci 'commit -a -m'
 git config --global alias.st status
@@ -31,27 +34,9 @@ git config --global alias.sdf diff --cached
 git config --global alias.lsstash 'stash list'
 git config --global alias.unstash 'stash pop'
 git config --global alias.wipeout 'checkout -- '*''
+git config --global alias.rbm 'rebase master'
+git config --global alias.info 'remote show origin'
 echo "Done"
 
 echo "*** All git aliases ***"
 git config --global --get-regexp alias*
-
-
-
-# # declare an array variable
-# declare -a arr=(
-# 'git config --global alias.co checkout'	
-# 'git config --global alias.cob '\'"checkout -b"\'
-# 'git config --global alias.br branch')
-
-
-# ## now loop through the above array
-# for i in "${arr[@]}"
-# do
-	# echo "$i"
-	# $i
-# done
-
-# echo "${arr[0]}"
-
-# # You can access them using echo "${arr[0]}", "${arr[1]}" also
